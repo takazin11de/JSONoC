791ac370c9cea659dbcd86b4c544e88d9959701a
tkzn <takazin.11de@gmail.com>
2025-04-28 19:23:58 +0900

    Add files via upload

--- jsonoc.js
+++ jsonoc.js
@@ -1,62 +1,34 @@
 class JSONoC {
-  static #iframe;
-  static fetch = async (url, timeout, key = ``) => {
-    const doc = document;
-    if (!this.#iframe) {
-      this.#iframe = doc.createElement(`iframe`);
-      this.#iframe.setAttribute(`style`, `visibility:hidden;position:fixed;`);
-      await new Promise((resolve) => {
-        this.#iframe.onload = () => {
-          resolve();
-        };
-        doc.body.appendChild(this.#iframe);
-      });
-    }
-    const p = doc.createElement(`p`);
-    const timer_task = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        p.remove();
-        reject(`timeout`);
-      }, timeout);
-    });
-    this.#iframe.contentDocument.body.appendChild(p);
-    const shadow = p.attachShadow({ mode: `open` });
-    const link = doc.createElement(`link`);
-    link.rel = `stylesheet`;
-    link.href = url;
-    const css_onload = new Promise((resolve, reject) => {
-      link.onload = () => {
-        try {
-          const content = getComputedStyle(
-            shadow.childNodes[1]
-          ).getPropertyValue(`--_`);
-          resolve(JSON.parse(content.trim().slice(1, -1)
-            .replaceAll(`$5`, '`')
-            .replaceAll(`$4`, `'`)
-            .replaceAll(`$3`, `\\`)
-            .replaceAll(`$2`, `\\"`)
-            .replaceAll(`$1`, `\\\\`)
-            .replaceAll(`$0`, `$`)
-          ));
-        } catch (e) {
-          reject(e);
-        }
-      };
-      link.onerror = () => { reject(`error`); };
-    });
-    shadow.appendChild(link);
-    const p2 = doc.createElement(`p`);
-    p2.id = `_${key}`;
-    shadow.appendChild(p2);
-    return await Promise.race([timer_task, css_onload]);
+  static #f;
+  static fetch = (url, timeout = 5000, key = ``) => {
+    const p = document.createElement(`p`);
+    return new Promise((resolve, reject) => {
+      (new Promise(resolve => {
+        if (this.#f) resolve();
+        document.body.append(Object.assign(this.#f = document.createElement(`iframe`),
+          { style: `visibility:hidden;position:fixed`, onload: _ => resolve() }))
+      })).then(_ => {
+        (new Promise((resolve, reject) => {
+          const t = Object.assign(document.createElement(`p`), { id: `_${key}` });
+          this.#f.contentDocument.body.appendChild(p).attachShadow({ mode: `open` }).append(
+            Object.assign(document.createElement(`link`), {
+              rel: `stylesheet`, href: url, onerror: _ => reject(`error`), onload: _ => {
+                try {
+                  resolve(JSON.parse(getComputedStyle(t).getPropertyValue(`--_`).trim().slice(1, -1)
+                    .replaceAll(`$5`, '`').replaceAll(`$4`, `'`).replaceAll(`$3`, `\\`)
+                    .replaceAll(`$2`, `\\"`).replaceAll(`$1`, `\\\\`).replaceAll(`$0`, `$`)
+                  ));
+                } catch (e) { reject(e) }
+              }
+            }), t);
+          setTimeout(_ => reject(`timeout`), timeout);
+        })).then(j => resolve(j)).catch(e => reject(e)).finally(_ => p.remove())
+      }).catch(_ => reject(`error`))
+    })
   };
   static cnv = (json, key = ``) => {
     return `#${CSS.escape(`_${key}`)}{--_:'${(JSON.stringify(json))
-      .replaceAll(`$`, `$0`)
-      .replaceAll(`\\\\`, `$1`)
-      .replaceAll(`\\"`, `$2`)
-      .replaceAll(`\\`, `$3`)
-      .replaceAll(`'`, `$4`)
-      .replaceAll('`', `$5`)}';}`;
-  };
+      .replaceAll(`$`, `$0`).replaceAll(`\\\\`, `$1`).replaceAll(`\\"`, `$2`)
+      .replaceAll(`\\`, `$3`).replaceAll(`'`, `$4`).replaceAll('`', `$5`)}'}`
+  }
 }
