63a61b6e1e0b3f11e1df5e52950808e53e803df2
tkzn <takazin.11de@gmail.com>
2025-04-29 01:15:14 +0900

    Add files via upload

--- jsonoc.js
+++ jsonoc.js
@@ -1,15 +1,19 @@
 class JSONoC {
   static #f;
+  static #l = [];
   static fetch = (url, timeout = 5000, key = ``) => {
-    const p = document.createElement(`p`);
     return new Promise((resolve, reject) => {
       (new Promise(resolve => {
-        if (this.#f) resolve();
+        this.#l.push([resolve, document.createElement(`p`), Object.assign(document.createElement(`p`), { id: `_${key}` })]);
+        if (this.#l[0] == 1) {
+          resolve(this.#l[1].slice(1, 3));
+          this.#l = [1];
+        }
+        if (this.#f) return;
         document.body.append(Object.assign(this.#f = document.createElement(`iframe`),
-          { style: `visibility:hidden;position:fixed`, onload: _ => resolve() }))
-      })).then(_ => {
+          { style: `visibility:hidden;position:fixed`, onload: _ => { this.#l.forEach(([a, b, c]) => a([b, c])); this.#l = [1] } }))
+      })).then(([p, t]) => {
         (new Promise((resolve, reject) => {
-          const t = Object.assign(document.createElement(`p`), { id: `_${key}` });
           this.#f.contentDocument.body.appendChild(p).attachShadow({ mode: `open` }).append(
             Object.assign(document.createElement(`link`), {
               rel: `stylesheet`, href: url, onerror: _ => reject(`error`), onload: _ => {
@@ -21,7 +25,7 @@ class JSONoC {
                 } catch (e) { reject(e) }
               }
             }), t);
-          setTimeout(_ => reject(`timeout`), timeout);
+          setTimeout(_ => { reject(`timeout`) }, timeout);
         })).then(j => resolve(j)).catch(e => reject(e)).finally(_ => p.remove())
       }).catch(_ => reject(`error`))
     })
