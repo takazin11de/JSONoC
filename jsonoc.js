a54749db511633eef3e211c45101d9a9a41156ff
tkzn <takazin.11de@gmail.com>
2025-04-29 01:40:44 +0900

    Add files via upload

--- jsonoc.js
+++ jsonoc.js
@@ -4,29 +4,26 @@ class JSONoC {
   static fetch = (url, timeout = 5000, key = ``) => {
     return new Promise((resolve, reject) => {
       (new Promise(resolve => {
-        this.#l.push([resolve, document.createElement(`p`), Object.assign(document.createElement(`p`), { id: `_${key}` })]);
-        if (this.#l[0] == 1) {
-          resolve(this.#l[1].slice(1, 3));
-          this.#l = [1];
-        }
+        this.#l.push([resolve, document.createElement(`p`), Object.assign(document.createElement(`p`), { id: `_${key}` }),
+          setTimeout(_ => { reject(`timeout`) }, timeout)]);
+        if (this.#l[0] == 1) { resolve(this.#l[1].slice(1, 4)); this.#l = [1] }
         if (this.#f) return;
         document.body.append(Object.assign(this.#f = document.createElement(`iframe`),
-          { style: `visibility:hidden;position:fixed`, onload: _ => { this.#l.forEach(([a, b, c]) => a([b, c])); this.#l = [1] } }))
-      })).then(([p, t]) => {
+          { style: `visibility:hidden;position:fixed`, onload: _ => { this.#l.forEach(([a, b, c, d]) => a([b, c, d])); this.#l = [1] } }))
+      })).then(([p, s, t]) => {
         (new Promise((resolve, reject) => {
           this.#f.contentDocument.body.appendChild(p).attachShadow({ mode: `open` }).append(
             Object.assign(document.createElement(`link`), {
               rel: `stylesheet`, href: url, onerror: _ => reject(`error`), onload: _ => {
                 try {
-                  resolve(JSON.parse(getComputedStyle(t).getPropertyValue(`--_`).trim().slice(1, -1)
+                  resolve(JSON.parse(getComputedStyle(s).getPropertyValue(`--_`).trim().slice(1, -1)
                     .replaceAll(`$5`, '`').replaceAll(`$4`, `'`).replaceAll(`$3`, `\\`)
                     .replaceAll(`$2`, `\\"`).replaceAll(`$1`, `\\\\`).replaceAll(`$0`, `$`)
                   ));
                 } catch (e) { reject(e) }
               }
-            }), t);
-          setTimeout(_ => { reject(`timeout`) }, timeout);
-        })).then(j => resolve(j)).catch(e => reject(e)).finally(_ => p.remove())
+            }), s)
+        })).then(j => resolve(j)).catch(e => reject(e)).finally(_ => { clearTimeout(t); p.remove() })
       }).catch(_ => reject(`error`))
     })
   };
